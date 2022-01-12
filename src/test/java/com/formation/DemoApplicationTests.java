322d250c3f0061f1c7a0337e786d656e1d8b5789
Formation67 <formation167@gmail.com>
2022-01-12 12:27:45 +0100

    Initial commit

---
+++ src/test/java/com/formation/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.formation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.formation.service.Calculator;
+
+@SpringBootTest
+class DemoApplicationTests {
+	
+	private Calculator calcul = new Calculator();
+
+	@Test
+	void testSum() {
+		
+		assertEquals(5, calcul.sum(2, 3));
+	}
+
+}
